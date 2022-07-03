e57ff97861e5dd3394b169fe90cc8debb4e54141
Andy James <kana@ikimi.co>
2022-07-03 21:16:31 +0100

    Add ToString to Location

--- FFXIVVenues.VenueModels/V2022/Location.cs
+++ FFXIVVenues.VenueModels/V2022/Location.cs
@@ -1,4 +1,6 @@
-﻿namespace FFXIVVenues.VenueModels.V2022
+﻿using System.Text;
+
+namespace FFXIVVenues.VenueModels.V2022
 {
     public class Location
     {
@@ -11,5 +13,36 @@
         public ushort Room { get; set; }
         public bool Subdivision { get; set; }
         public string Shard { get; set; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(this.DataCenter);
+            stringBuilder.Append(", ");
+            stringBuilder.Append(this.World);
+            stringBuilder.Append(", ");
+            stringBuilder.Append(this.District);
+            stringBuilder.Append(", Ward ");
+            stringBuilder.Append(this.Ward);
+            if (this.Apartment != 0 && this.Subdivision)
+                stringBuilder.Append(" Sub");
+            if (this.Plot != 0)
+            {
+                stringBuilder.Append(", Plot ");
+                stringBuilder.Append(this.Plot);
+                if (this.Room != 0)
+                {
+                    stringBuilder.Append(", Room ");
+                    stringBuilder.Append(this.Room);
+                }
+            }
+            else
+            {
+                stringBuilder.Append(", Apt ");
+                stringBuilder.Append(this.Apartment);
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }

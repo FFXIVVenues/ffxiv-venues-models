cf8ade7bde0c684eae2fff442f047a6b60db9042
kana <kana@ffxivvenues.com>
2023-05-08 23:47:20 +0100

    Change classes to records

--- FFXIVVenues.VenueModels/Observability/Observation.cs
+++ FFXIVVenues.VenueModels/Observability/Observation.cs
@@ -1,9 +1,4 @@
 namespace FFXIVVenues.VenueModels.Observability
 {
-    public class Observation
-    {
-        public ObservableOperation Operation { get; set; }
-        public string SubjectId { get; set; }
-        public string SubjectName { get; set; }
-    }
+    public record Observation(ObservableOperation Operation, string SubjectId, string SubjectName);
 }
--- FFXIVVenues.VenueModels/Observability/ObserveRequest.cs
+++ FFXIVVenues.VenueModels/Observability/ObserveRequest.cs
@@ -2,12 +2,8 @@ using System.Collections.Generic;
 
 namespace FFXIVVenues.VenueModels.Observability
 {
-    public class ObserveRequest
-    {
-        public ObservableKey? KeyCriteria { get; set; }
-
-        public string ValueCriteria { get; set; }
-
-        public IEnumerable<ObservableOperation> OperationCriteria { get; set; }
-    }
+    public record ObserveRequest(
+        IEnumerable<ObservableOperation> OperationCriteria, 
+        ObservableKey? KeyCriteria,
+        string ValueCriteria);
 }

053e157972fe738ff601032e9fd8696d57aa05e0
kana <kana@ffxivvenues.com>
2023-05-09 03:30:43 +0100

    Change observe request to class and provide match method

--- FFXIVVenues.VenueModels/Observability/ObserveRequest.cs
+++ FFXIVVenues.VenueModels/Observability/ObserveRequest.cs
@@ -1,9 +1,36 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FFXIVVenues.VenueModels.Observability
 {
-    public record ObserveRequest(
-        IEnumerable<ObservableOperation> OperationCriteria, 
-        ObservableKey? KeyCriteria,
-        string ValueCriteria);
+    public class ObserveRequest
+    {
+        
+        public ObserveRequest(IEnumerable<ObservableOperation> operationCriteria, 
+            ObservableKey? keyCriteria, string valueCriteria)
+        {
+            this.OperationCriteria = operationCriteria;
+            this.KeyCriteria = keyCriteria;
+            this.ValueCriteria = valueCriteria;
+        }
+
+        public IEnumerable<ObservableOperation> OperationCriteria { get; set; }
+        public ObservableKey? KeyCriteria { get; set; }
+        public string ValueCriteria { get; set; }
+        
+        public bool Matches(ObservableOperation operation, VenueModels.Venue venue)
+        {
+            if (venue == null) return false;
+            if (!OperationCriteria.Contains(operation)) return false;
+            return KeyCriteria switch
+            {
+                ObservableKey.Id => venue.Id == ValueCriteria,
+                ObservableKey.DataCenter => venue.Location.DataCenter == ValueCriteria,
+                ObservableKey.World => venue.Location.World == ValueCriteria,
+                ObservableKey.Manager => venue.Managers.Contains(ValueCriteria),
+                _ => true,
+            };
+        }
+
+    };
 }

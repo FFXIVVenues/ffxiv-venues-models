3af6a23684b240dd5bcfd4cf8317043809c69ee9
Kana Ki <kana@ksharp.net>
2023-06-20 03:06:42 +0100

    Remove redundant field in VenueModels

--- FFXIVVenues.VenueModels/Time.cs
+++ FFXIVVenues.VenueModels/Time.cs
@@ -7,7 +7,6 @@ namespace FFXIVVenues.VenueModels
     {
         private ushort _hour;
         private ushort _minute;
-        private string _timeZone = TimeZoneInfo.Utc.Id;
 
         public ushort Hour { get => _hour; set { if (value < 0 || value > 23) throw new ArgumentOutOfRangeException(nameof(Hour), $"Hour must be set to an integer between 0 and 23 (inclusive)."); _hour = value; } }
         public ushort Minute { get => _minute; set { if (value < 0 || value > 59) throw new ArgumentOutOfRangeException(nameof(Minute), $"Minute must be set to an integer between 0 and 59 (inclusive)."); _minute = value; } }
